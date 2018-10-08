e1013f90e2d9ec5742256f916c82d637562d0590
Rodolfo Carlos Zavala Sandoval <rodolfocarlos.zavala@bbva.com>
2018-10-08 18:41:42 -0500

    primer subidad

---
+++ simple/js/input.js
@@ -0,0 +1,25 @@
+$('input').focus(function(){
+  $(this).parents('.form-group').addClass('focused');
+});
+
+$('input').blur(function(){
+  var inputValue = $(this).val();
+  if ( inputValue == "" ) {
+    $(this).removeClass('filled');
+    $(this).parents('.form-group').removeClass('focused');  
+  } else {
+    $(this).addClass('filled');
+  }
+})  
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+  this.getElementById('loadMore').addEventListener('click', function() {
+
+      
+  });
+    
+    
+});
